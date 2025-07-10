b649759464d45a732c0c0da13a2d9ef79703c122
Fidelisaboke <fidelisaboke@gmail.com>
2025-07-10 13:12:01 +0300

    feat: update login page - Add 'Sign in with Google' and 'Sign in with GitHub' buttons - Add 'Register' link after the 'Log In' button

--- src/pages/Login/page.tsx
+++ src/pages/Login/page.tsx
@@ -10,6 +10,7 @@ import { Checkbox } from '@/components/ui/checkbox'
 import { Label } from '@/components/ui/label'
 import { Eye, EyeOff, Mail } from 'lucide-react'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { FaGoogle, FaGithub } from "react-icons/fa"
 
 import { type LoginSchema, loginSchema } from '@/schemas/login'
 
@@ -33,7 +34,25 @@ export default function LoginPage() {
             <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
                 <Form {...form}>
                     <h2 className="text-2xl font-bold mb-4 text-center">Welcome Back!</h2>
-                    <p className="text-sm text-center text-gray-600 mb-8">Please enter your details to login.</p>
+                    <p className="text-sm text-gray-600 mb-4">Sign in to your account to continue.</p>
+                    {/* Google Sign In */}
+                    <div className="w-full flex flex-col items-center justify-center">
+                        <Button type="button" className="mb-2 w-full border border-gray-300 bg-gray-100 hover:bg-gray-200 text-gray-900">
+                            <FaGoogle />
+                            Sign in with Google
+                        </Button>
+                        <Button type="button" className="mb-2 w-full border border-gray-300 bg-gray-100 hover:bg-gray-200 text-gray-900">
+                            <FaGithub />
+                            Sign in with GitHub
+                        </Button>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <div className="w-full h-[1px] bg-gray-300 my-4"></div>
+                        <span className="px-2 text-gray-500">OR</span>
+                        <div className="w-full h-[1px] bg-gray-300 my-4"></div>
+                    </div>
+
+
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         {/* Email Address */}
                         <FormField
@@ -59,10 +78,10 @@ export default function LoginPage() {
                                     <FormLabel>Password</FormLabel>
                                     <FormControl>
                                         <div className="relative">
-                                            <Input 
-                                                type={showPassword ? "text" : "password"} 
-                                                placeholder="********" 
-                                                {...field} 
+                                            <Input
+                                                type={showPassword ? "text" : "password"}
+                                                placeholder="********"
+                                                {...field}
                                             />
                                             <button
                                                 type="button"
@@ -78,7 +97,7 @@ export default function LoginPage() {
                             )}
                         />
 
-                        <div className="flex items-center justify-between">
+                        <div className="flex flex-row flex-col items-center justify-between">
                             <div className="flex items-center">
                                 <Checkbox id="remember" />
                                 <Label htmlFor="remember" className="ml-2">Remember me</Label>
@@ -94,6 +113,13 @@ export default function LoginPage() {
                         <Button type="submit" className="w-full bg-teal-600 hover:bg-teal-700 text-white">
                             Log In
                         </Button>
+
+                        <p className="text-sm text-gray-600 text-center">
+                            Don&apos;t have an account?
+                            <Link to="/register" className="text-teal-600 hover:underline hover:text-teal-700 ml-1">
+                                Register
+                            </Link>
+                        </p>
 
                     </form>
                 </Form>
