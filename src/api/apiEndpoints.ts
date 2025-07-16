9b59c5aa09558f6b79545458848e7a938e6abccd
Fidelisaboke <fidelisaboke@gmail.com>
2025-07-16 18:06:46 +0300

    fix: ensure endpoints are correct

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -1,11 +1,9 @@
 // Backend API Endpoints
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
 const API_ENDPOINTS = {
     AUTH: {
-        LOGIN: `${BASE_URL}/auth/login/`,
-        REGISTER: `${BASE_URL}/auth/register/`,
-        PROFILE: `${BASE_URL}/auth/me/`,
+        LOGIN: '/auth/login/',
+        REGISTER: '/auth/register/',
+        PROFILE: '/auth/me/',
     },
 } as const;
 
