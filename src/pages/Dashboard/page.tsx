8d2b071364a0763e548cd46affd641cd2c5b31e6
Fidelisaboke <fidelisaboke@gmail.com>
2025-08-05 13:24:14 +0300

    feat: add dashboard metrics

--- src/pages/Dashboard/page.tsx
+++ src/pages/Dashboard/page.tsx
@@ -1,14 +1,38 @@
 import AppLayout from "@/components/AppLayout.tsx";
+import { useBudgets } from "@/hooks/useBudgets";
+import { useTransactions } from "@/hooks/useTransactions";
+import { useCategories } from "@/hooks/useCategories";
+import LoadingPage from "@/components/LoadingPage.tsx";
+import {aggregateDashboardMetrics} from "@/utils/dashboardMetrics.ts";
+
 
 export default function Dashboard() {
+    const { data: budgetsData, isLoading: budgetsLoading } = useBudgets();
+    const { data: transactionsData, isLoading: transactionsLoading } = useTransactions();
+    const { data: categoriesData, isLoading: categoriesLoading } = useCategories();
+
+    if (budgetsLoading || transactionsLoading || categoriesLoading) {
+        return <LoadingPage />;
+    }
+
+    const budgets = budgetsData?.items || [];
+    const transactions = transactionsData?.items || [];
+    const categories = categoriesData?.items || [];
+
+    // Dashboard Metrics
+    const metrics = aggregateDashboardMetrics(
+        transactions,
+        budgets,
+        categories
+    );
+
     return (
         <AppLayout title={"Dashboard Overview"}>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8">
-                {/* TODO: Add metrics cards here */}
-                <OverviewCard title={"Total Income"} value={`Ksh. ${0.00}`} />
-                <OverviewCard title={"Total Expenses"} value={`Ksh. ${0.00}`} />
-                <OverviewCard title={"Budget Usage"} value={`${0.00}%`} />
-                <OverviewCard title={"Categories"} value={`${0}`} />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
+                <OverviewCard title={"Total Income"} value={`Ksh. ${metrics.totalIncome}`} />
+                <OverviewCard title={"Total Expenses"} value={`Ksh. ${metrics.totalExpenses}`} />
+                <OverviewCard title={"Budget Usage"} value={`${metrics.budgetUsage.toFixed(2)}%`} />
+                <OverviewCard title={"Categories"} value={`${metrics.categoryCount}`} />
             </div>
             <div className="bg-white rounded shadow p-4">
                 <div className="text-lg font-semibold mb-2 text-teal-700">Recent Transactions</div>
