21ed85d3841c094ea3b9dccae5c7911482cb37fc
Fidelisaboke <fidelisaboke@gmail.com>
2025-08-06 20:17:14 +0300

    feat: add categories page scaffolding

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -10,7 +10,7 @@ export default function AppLayout({title, children} :{title: string, children: R
       <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
       <div className="flex flex-1">
         <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
-        <main className="flex-1 p-6 w-full">
+        <main className="flex-1 p-6 w-full max-w-7xl">
           <h2 className="text-2xl font-bold mb-6 text-teal-700">{title}</h2>
             {children}
         </main>
