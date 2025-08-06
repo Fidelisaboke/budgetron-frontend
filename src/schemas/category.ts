c4ab9eb2ede38f2c12b34e5f55f70f0009a51d38
Fidelisaboke <fidelisaboke@gmail.com>
2025-08-06 23:36:27 +0300

    feat: update category schema to match backend

--- src/schemas/category.ts
+++ src/schemas/category.ts
@@ -4,7 +4,9 @@ export const categorySchema = z.object({
     id: z.number().int().optional(),
     name: z.string().min(2).max(50),
     type: z.enum(['income', 'expense']),
-    last_updated: z.string().datetime().optional(),
+    is_default: z.boolean(),
+    created_at: z.string().datetime().optional(),
+    updated_at: z.string().datetime().optional(),
 })
 
 export type Category = z.infer<typeof categorySchema>;
