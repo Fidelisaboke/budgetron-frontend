ac713399c7563a96e056570314ccce22161f0ec2
Fidelisaboke <fidelisaboke@gmail.com>
2025-08-07 12:42:47 +0300

    feat: update add category dialog

--- src/components/dialogs/AddCategoryDialog.tsx
+++ src/components/dialogs/AddCategoryDialog.tsx
@@ -1,14 +1,3 @@
-import {
-    Dialog,
-    DialogTrigger,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogDescription,
-    DialogFooter
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-
 export default function AddCategoryDialog() {
 
 }
